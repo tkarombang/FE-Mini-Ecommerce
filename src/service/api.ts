dc278707c488b4a6a42d151fabdb429bad9c6c2d
tkarombang <tuangkarombang@gmail.com>
2025-08-18 13:12:56 +0800

    Deploy - Process part1

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL_MINIECOM_RK || "http://localhost:8000",
   headers: {
     // Perbaikan: "application/json"
     "Content-Type": "application/json",
